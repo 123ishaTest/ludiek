f31cc8c844a9f694e8b4a98220d4bcd1f842b30e
Isha <ishadijcks@gmail.com>
2025-07-25 16:41:32 +0200

    Use Vite as bundler for ludiek (#54)
    * Use Vite as bundler for ludiek

---
+++ packages/ludiek/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), dts()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'ludiek',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+    },
+  },
+  server: {
+    fs: {
+      allow: ['.'],
+    },
+  },
+});
