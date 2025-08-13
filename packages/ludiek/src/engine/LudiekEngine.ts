c3a7249fd513d11115ded357056d23ba19f9a614
Isha Dijcks <ishadijcks@gmail.com>
2025-08-13 20:34:28 +0200

    Improve testing

--- packages/ludiek/src/engine/LudiekEngine.ts
+++ packages/ludiek/src/engine/LudiekEngine.ts
@@ -1,10 +1,11 @@
 import { LudiekConfig, PluginMap } from '@ludiek/engine/LudiekConfiguration';
 import { LudiekPlugin } from '@ludiek/engine/LudiekPlugin';
 import { BaseConditionShape, ConditionShape, LudiekCondition } from '@ludiek/engine/LudiekCondition';
+import { ConditionNotFoundError } from '@ludiek/engine/LudiekError';
 
 export class LudiekEngine<Plugins extends LudiekPlugin[], Conditions extends LudiekCondition<BaseConditionShape>[]> {
   public plugins: PluginMap<Plugins>;
-  private _conditions: Record<string, LudiekCondition<BaseConditionShape>>;
+  private readonly _conditions: Record<string, LudiekCondition<BaseConditionShape>>;
 
   constructor(config: LudiekConfig<Plugins, Conditions>) {
     this.plugins = Object.fromEntries(config.plugins?.map((p) => [p.name, p]) ?? []) as PluginMap<Plugins>;
@@ -16,6 +17,15 @@ export class LudiekEngine<Plugins extends LudiekPlugin[], Conditions extends Lud
    * Evaluate a condition and return whether it is true.
    */
   public evaluate(condition: ConditionShape<Conditions>): boolean {
+    const evaluator = this._conditions[condition.type];
+
+    if (evaluator == null) {
+      const registeredEvaluators = Object.keys(this._conditions).join(', ');
+      throw new ConditionNotFoundError(
+        `Cannot evaluate condition of type '${condition.type}' because its evaluator is not registered. Registered evaluators are ${registeredEvaluators}`,
+      );
+    }
+
     return this._conditions[condition.type].evaluate(condition);
   }
 }
--- packages/ludiek/tests/engine/conditions.spec.ts
+++ packages/ludiek/tests/engine/conditions.spec.ts
@@ -1,19 +1,17 @@
 import { describe, expect, it } from 'vitest';
 import { LudiekEngine } from '@ludiek/engine/LudiekEngine';
 import { LudiekCondition } from '@ludiek/engine/LudiekCondition';
-
-class AlwaysTrueEvaluator implements LudiekCondition<never> {
-  type: string = 'always-true';
-  evaluate(): boolean {
-    return true;
-  }
-}
+import { ConditionNotFoundError } from '@ludiek/engine/LudiekError';
 
 describe('Engine Conditions', () => {
   it('checks an always true condition', () => {
     // Arrange
+    const alwaysTrueEvaluator = {
+      type: 'always-true',
+      evaluate: () => true,
+    };
     const engine = new LudiekEngine({
-      conditions: [new AlwaysTrueEvaluator()],
+      conditions: [alwaysTrueEvaluator],
     });
 
     // Act
@@ -21,5 +19,77 @@ describe('Engine Conditions', () => {
 
     // Assert
     expect(result).toBe(true);
+  });
+
+  it('checks an always false condition', () => {
+    // Arrange
+    const alwaysFalseEvaluator = {
+      type: 'always-false',
+      evaluate: () => false,
+    };
+    const engine = new LudiekEngine({
+      conditions: [alwaysFalseEvaluator],
+    });
+
+    // Act
+    const result = engine.evaluate({ type: 'always-false' });
+
+    // Assert
+    expect(result).toBe(false);
+  });
+
+  it('checks a condition', () => {
+    // Arrange
+
+    interface MyShape {
+      type: 'maybe';
+      value: number;
+    }
+
+    interface MyFeature {
+      points: number;
+    }
+
+    class MyFeatureEvaluator implements LudiekCondition<MyShape> {
+      type = 'maybe';
+      private _feature: MyFeature;
+
+      constructor(feature: MyFeature) {
+        this._feature = feature;
+      }
+
+      evaluate(condition: MyShape): boolean {
+        return this._feature.points >= condition.value;
+      }
+    }
+
+    const myFeature: MyFeature = {
+      points: 4,
+    };
+
+    const engine = new LudiekEngine({
+      conditions: [new MyFeatureEvaluator(myFeature)],
+    });
+
+    // Act
+    const has4 = engine.evaluate({ type: 'maybe', value: 4 });
+    const has5 = engine.evaluate({ type: 'maybe', value: 5 });
+    myFeature.points = 3;
+    const has4Again = engine.evaluate({ type: 'maybe', value: 4 });
+
+    // Assert
+    expect(has4).toBe(true);
+    expect(has5).toBe(false);
+    expect(has4Again).toBe(false);
+  });
+
+  it('errors when an evaluator does not exist', () => {
+    // Arrange
+    const engine = new LudiekEngine({});
+
+    // Act
+    expect(() => {
+      engine.evaluate({ type: 'wrong' });
+    }).toThrow(ConditionNotFoundError);
   });
 });
