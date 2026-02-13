045457adcbabaa5a7c98683b8fd82fc93d0af8c0
Isha <ishadijcks@gmail.com>
2026-02-13 21:45:08 +0100

    Major documentation rework (#140)
    * Update documentation
    
    * Major documentation rework

--- apps/website/src/lib/docs.ts
+++ apps/website/src/lib/docs.ts
@@ -1,23 +1,21 @@
-import type { DocsLayout } from '$lib/model/Docs';
+import type { DocsLayout, DocsPage } from '$lib/model/Docs';
 
 export const docsLayout: DocsLayout = {
   categories: [
     {
       title: 'Introduction',
-      pages: [
-        { title: 'Welcome', slug: 'welcome' },
-        { title: 'Getting started', slug: 'getting-started' },
-      ],
+      pages: [{ title: 'Welcome', slug: 'introduction/welcome' }],
     },
     {
-      title: 'Engine',
+      title: 'Getting Started',
       pages: [
-        { title: 'Conditions', slug: 'engine/conditions' },
-        { title: 'Input', slug: 'engine/input' },
-        { title: 'Output', slug: 'engine/output' },
-        { title: 'Requests', slug: 'engine/requests' },
-        { title: 'Bonuses', slug: 'engine/bonuses' },
-        { title: 'Persistence', slug: 'engine/persistence' },
+        { title: 'Overview', slug: 'getting-started/overview' },
+        { title: 'Setting Up', slug: 'getting-started/setting-up' },
+        { title: 'Adding a Plugin', slug: 'getting-started/adding-a-plugin' },
+        { title: 'Content', slug: 'getting-started/content' },
+        { title: 'Engine Concepts', slug: 'getting-started/engine-concepts' },
+        { title: 'Our first feature', slug: 'getting-started/our-first-feature' },
+        { title: "What's next?", slug: 'getting-started/whats-next' },
       ],
     },
     {
@@ -34,5 +32,45 @@ export const docsLayout: DocsLayout = {
         { title: 'Upgrade', slug: 'plugins/upgrade' },
       ],
     },
+    {
+      title: 'Engine',
+      pages: [
+        { title: 'Conditions', slug: 'engine/conditions' },
+        { title: 'Input', slug: 'engine/input' },
+        { title: 'Output', slug: 'engine/output' },
+        { title: 'Requests', slug: 'engine/requests' },
+        { title: 'Bonuses', slug: 'engine/bonuses' },
+        { title: 'Persistence', slug: 'engine/persistence' },
+      ],
+    },
+    {
+      title: 'Complementaries',
+      pages: [
+        { title: 'Lui', slug: 'complementaries/lui' },
+        { title: 'Lyrisch', slug: 'complementaries/lyrisch' },
+      ],
+    },
+    {
+      title: 'Other',
+      pages: [{ title: 'Roadmap', slug: 'other/roadmap' }],
+    },
   ],
+};
+
+export const getNext = (slug: string): DocsPage | undefined => {
+  for (const category of docsLayout.categories) {
+    const index = category.pages.findIndex((page) => page.slug === slug);
+    if (index != -1) {
+      return category.pages.length - 1 === index ? undefined : category.pages[index + 1];
+    }
+  }
+};
+
+export const getPrevious = (slug: string): DocsPage | undefined => {
+  for (const category of docsLayout.categories) {
+    const index = category.pages.findIndex((page) => page.slug === slug);
+    if (index != -1) {
+      return 0 === index ? undefined : category.pages[index - 1];
+    }
+  }
 };
--- apps/website/src/routes/docs/[...slug]/+page.svelte
+++ apps/website/src/routes/docs/[...slug]/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import { getNext, getPrevious } from '$lib/docs';
+
   let { data } = $props();
+
+  let slug = $derived(data.slug);
+  let previous = $derived(getPrevious(slug));
+  let next = $derived(getNext(slug));
 </script>
 
 <svelte:head>
@@ -12,4 +18,31 @@
   <h1 class="text-primary">{data.meta.title}</h1>
 
   <data.content />
+
+  <div class="mt-8 flex flex-row justify-between space-x-8">
+    {#if previous}
+      <!-- eslint-disable svelte/no-navigation-without-resolve-->
+      <a
+        href="/docs/{previous.slug}"
+        class="border-base-content/20 hover:border-base-content/30 flex grow flex-col rounded-xl border p-4 no-underline"
+      >
+        <span class="text-xs">Previous page</span>
+        <span class="text-primary">{previous.title}</span>
+      </a>
+    {:else}
+      <div class="grow"></div>
+    {/if}
+
+    {#if next}
+      <a
+        href="/docs/{next.slug}"
+        class="border-base-content/20 hover:border-base-content/30 flex grow flex-col items-end rounded-xl border p-4 no-underline"
+      >
+        <span class="text-xs">Next page</span>
+        <span class="text-primary">{next.title}</span>
+      </a>
+    {:else}
+      <div class="grow"></div>
+    {/if}
+  </div>
 </article>
