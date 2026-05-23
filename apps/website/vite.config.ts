714a99fd4891d2f7b405272bd2afd0dda0441994
Isha Dijcks <ishadijcks@gmail.com>
2026-05-23 21:55:21 +0200

    Remove path

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,13 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'node:path';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
-  resolve: {
-    alias: {
-      '@123ishatest/lui': path.resolve(__dirname, '../../packages/lui/src'),
-    },
-  },
 });
