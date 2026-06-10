a6d029df9aa364fa4d57e45434202fb8a7791826
Isha Dijcks <ishadijcks@gmail.com>
2026-06-11 00:11:04 +0200

    Export LuiToolbarEntry

--- packages/lui/src/lib/index.ts
+++ packages/lui/src/lib/index.ts
@@ -4,6 +4,7 @@ export { default as LuiDebugger } from '$lui/pages/LuiDebugger.svelte';
 export { default as LuiContentCard } from '$lui/components/content/LuiContentCard.svelte';
 export { default as LuiContentHint } from '$lui/components/content/LuiContentHint.svelte';
 export { default as LuiContext } from '$lui/LuiContext.svelte';
+export { default as LuiToolbarEntry } from '$lui/components/toolbar/LuiToolbarEntry.svelte';
 
 export type { LuiConfig } from '$lui/LuiConfig';
 export { LuiConfigBuilder } from '$lui/LuiConfigBuilder';
